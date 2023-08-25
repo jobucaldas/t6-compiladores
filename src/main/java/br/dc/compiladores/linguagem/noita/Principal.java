9a641550a6f0090ffaaa84ddb9d10076f742c629
HotdogcNutela <mvsoppp@gnail.com>
2023-08-25 09:58:41 -0300

    Classe Principal implementada

--- src/main/java/br/dc/compiladores/linguagem/noita/Principal.java
+++ src/main/java/br/dc/compiladores/linguagem/noita/Principal.java
@@ -1,5 +1,22 @@
 package br.dc.compiladores.linguagem.noita;
 
+import br.dc.compiladores.linguagem.noita.NoitaParser.ProgramaContext;
+import java.io.IOException;
+
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+
 public class Principal {
-    
+    public static void main(String args[]) throws IOException{
+        CharStream cs = CharStreams.fromFileName(args[0]);
+        NoitaLexer lexer = new NoitaLexer(cs);
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        NoitaParser parser = new NoitaParser(tokens);
+        ProgramaContext arvore = parser.programa();
+        LinguagemNoita ln = new LinguagemNoita();
+        ln.visitPrograma(arvore);
+        LinguagemNoitaUtils.errosSemanticos.forEach((s) -> System.out.println(s));
+        System.out.println("Fim da compilação!");
+    }
 }
